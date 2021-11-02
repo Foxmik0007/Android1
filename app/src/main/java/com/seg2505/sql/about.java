0737368c50ee0df5d3e9b3aeba0566dab6e8751c
Mika Rafaralahy <mrafa099@uottawa.ca>
2021-11-02 17:15:38 -0500

    com

---
+++ app/src/main/java/com/seg2505/sql/about.java
@@ -0,0 +1,14 @@
+package com.seg2505.sql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class about extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
